e809f46898e6fd60c7bc8cb26247eb79f7bbf8de
Mimoza Mickoska <63194172+Mimziphery@users.noreply.github.com>
2023-05-20 22:53:27 +0200

    navbar implemented

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="bg-white border-gray-200 dark:bg-[#141414]">
+        <div class="max-w-screen-xl flex flex-wrap  content-center items-center justify-between mx-auto p-4 relative">
+            <a href="https://flowbite.com/" class="flex items-center content-center m-auto">
+                <ApplicationLogo class="w-32"/>
+            </a>
+            <div class="w-64 absolute">
+                <div class="relative">
+                    <SearchIcon class="ml-2 w-5 mt-1.5 absolute"/>
+                    <input type="text" class="rounded bg-[#2D2D2D] border-[#2D2D2D] w-64 h-8 pl-10 text-white"
+                           placeholder="Search topics"/>
+                </div>
+            </div>
+            <div class="float-right relative w-64">
+                <div class="m-auto">
+                    <UserIcon class="w-7 inline-block"/>
+                    <span class="text-white px-2 mt-1.5">Username</span>
+                    <AngleDownIcon class="inline-block w-4"/>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+import ApplicationLogo from "@/Components/Icons/ApplicationLogo.vue";
+import SearchIcon from "@/Components/Icons/SearchIcon.vue";
+import UserIcon from "@/Components/Icons/UserIcon.vue";
+import AngleDownIcon from "@/Components/Icons/AngleDownIcon.vue";
+
+export default {
+    components: {AngleDownIcon, UserIcon, SearchIcon, ApplicationLogo}
+}
+</script>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import {ref} from 'vue';
+import ApplicationLogo from '@/Components/Icons/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import {Link} from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -39,8 +39,6 @@ const showingNavigationDropdown = ref(false);
                         </div>
 
 
-
-
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- Settings Dropdown -->
                             <div class="ml-3 relative">
@@ -70,7 +68,7 @@ const showingNavigationDropdown = ref(false);
                                     </template>
 
                                     <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                                        <DropdownLink :href="route('profile.edit')"> Profile</DropdownLink>
                                         <DropdownLink :href="route('logout')" method="post" as="button">
                                             Log Out
                                         </DropdownLink>
@@ -133,7 +131,7 @@ const showingNavigationDropdown = ref(false);
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('profile.edit')"> Profile</ResponsiveNavLink>
                             <ResponsiveNavLink :href="route('logout')" method="post" as="button">
                                 Log Out
                             </ResponsiveNavLink>
@@ -145,13 +143,13 @@ const showingNavigationDropdown = ref(false);
             <!-- Page Heading -->
             <header class="bg-white shadow" v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
+                    <slot name="header"/>
                 </div>
             </header>
 
             <!-- Page Content -->
             <main>
-                <slot />
+                <slot/>
             </main>
         </div>
     </div>
